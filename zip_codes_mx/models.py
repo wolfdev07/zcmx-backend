3ba73e1d80aa67fedf8290b974f4042b23a92bd7
wolf <wolf@wolfbian.168.1.6>
2024-06-12 14:09:18 -0600

    [UPDATE] model settlement update

--- zip_codes_mx/models.py
+++ zip_codes_mx/models.py
@@ -67,8 +67,9 @@ class PostalCode(models.Model):
 class Settlement(models.Model):
     postal_code = models.ForeignKey(PostalCode, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    search_vector = SearchVectorField()
     settlement_type = models.CharField(max_length=255)
+    search_vector = SearchVectorField()
+    city = models.ManyToManyField(City, null=True, blank=True)
 
     class Meta:
         indexes = [
